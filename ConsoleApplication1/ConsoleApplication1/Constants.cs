b3651014315fea52f743d0fc39da0f2138478782
Alain Mereaux <amereaux@talentsoft.com>
2015-05-12 11:07:03 +0200

    Initial commit

---
+++ ConsoleApplication1/ConsoleApplication1/Constants.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ConsoleApplication1
+{
+    class Constants
+    {
+        public static class AppSettingKeys
+        {
+            public const string MessageContent = "Message.Content";
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Configuration;
+using System.Globalization;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = String.Format(CultureInfo.CurrentUICulture, "Message : {0}",
+                ConfigurationManager.AppSettings[Constants.AppSettingKeys.MessageContent]);
+            Console.WriteLine(message);
+            Console.WriteLine();
+            Console.WriteLine("Press ENTER to exit");
+            Console.ReadLine();
+        }
+    }
+}
